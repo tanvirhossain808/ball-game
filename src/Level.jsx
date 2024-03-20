399655eb689099c2d4d507185a5cf5af2c7f4dcf
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-20 22:46:52 +0600

    add random trap

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,18 +1,42 @@
+const Bounds = ({ length = 1 }) => {
 
 
-;
+}
+
+
+import { useMemo } from "react";
 import BlockStart, { BlockAxe, BlockEnd, BlockLimbo, BlockSpinner } from "./Block/BlockStart";
 
-const Level = () => {
+const Level = ({ count = 5, types = [BlockSpinner, BlockAxe, BlockLimbo] }) => {
+    const blocks = useMemo(() => {
+        const blocks = []
+
+
+        for (let i = 0; i < count; i++) {
+            const type = types[Math.floor(Math.random() * types.length)]
+            blocks.push(type)
+        }
+
+        return blocks
+
+    }, [count, types])
+    console.log(blocks);
     return (
         <>
 
-            <BlockStart position={[0, 0, 16]} />
+            {/* <BlockStart position={[0, 0, 16]} />
             <BlockSpinner position={[0, 0, 12]} />
             <BlockLimbo position={[0, 0, 8]} />
             <BlockAxe position={[0, 0, 4]} />
-            <BlockEnd position={[0, 0, 0]} />
-            {/* <BlockStart position={[0, 0, 0]} /> */}
+            <BlockEnd position={[0, 0, 0]} /> */}
+            <BlockStart position={[0, 0, 0]} />
+            {
+                blocks.map((Block, index) => (
+                    <Block key={index} position={[0, 0, -(index + 1) * 4]} />
+                ))
+            }
+            <BlockEnd position={[0, 0, -(count + 1) * 4]} />
+            <Bounds length={count + 2} />
         </>
     );
 };
