f63debd0e676acec0f7fb569044ae76adbd123ff
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-21 02:28:45 +0600

    add stopwatch and restart button for the gamem

---
+++ src/Interface.jsx
@@ -0,0 +1,15 @@
+const Interface = () => {
+    return (
+        <div className="interface">
+            <div className="time">
+                0.00
+            </div>
+            <div className="restart">
+                Restart
+            </div>
+
+        </div>
+    );
+};
+
+export default Interface;
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import { KeyboardControls } from '@react-three/drei'
+import Interface from './Interface.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -29,5 +30,6 @@ root.render(
         >
             <Experience />
         </Canvas>
+        <Interface />
     </KeyboardControls>
 )
