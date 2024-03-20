4f426353114d85a36bb9b0acd058802a1e06a5e4
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-21 02:46:05 +0600

    add button interface

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,4 +1,7 @@
+import { useKeyboardControls } from "@react-three/drei";
+
 const Interface = () => {
+    const { forward, backward, leftward, rightward, jump } = useKeyboardControls((state) => state)
     return (
         <div className="interface">
             <div className="time">
@@ -7,6 +10,22 @@ const Interface = () => {
             <div className="restart">
                 Restart
             </div>
+
+            <div className="controls">
+                <div className="raw">
+                    <div className={`key ${forward ? 'active' : ''}`}></div>
+                </div>
+                <div className="raw">
+                    <div className={`key ${leftward ? 'active' : ''}`}></div>
+                    <div className={`key ${backward ? 'active' : ''}`}></div>
+                    <div className={`key ${rightward ? 'active' : ''}`}></div>
+                </div>
+                <div className="raw">
+                    <div className={`key large ${jump ? 'active' : ''}`}></div>
+                </div>
+            </div>
+
+
 
         </div>
     );
